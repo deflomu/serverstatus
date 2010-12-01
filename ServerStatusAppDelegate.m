a1c490e2002b63592e49d882a6cf3e21e1e94300
Florian Mutter <elm@skweez.net>
2010-12-01 19:01:05 +0100

    Added message on first launch

--- ServerStatusAppDelegate.m
+++ ServerStatusAppDelegate.m
@@ -9,18 +9,47 @@
 #import "ServerStatusAppDelegate.h"
 #import "PreferenceWindowController.h"
 #import "ServerListController.h"
-#import "StatusItemController.h"
 #import "NetworkMonitor.h"
+#import "AutostartManager.h"
 
 
 @implementation ServerStatusAppDelegate
 @synthesize serverListController, statusItemController, suupdater;
 
 #pragma mark -
+#pragma mark First Launch
+- (void)firstLaunch {
+	DLog(@"First launch of ServerStatus");
+	
+	NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+	
+	if (![[AutostartManager sharedAutostartManager] isStartingAtLogin]) {
+		NSInteger ret = NSRunAlertPanel(@"Start ServerStatus automatically when you log in?",
+										@"To allow ServerStatus to check your servers automatically, it should be started when you login.",
+										@"Yes", @"No", NULL);
+		switch (ret){
+			case NSAlertDefaultReturn:
+				[[AutostartManager sharedAutostartManager] startAtLogin:YES];
+				break;
+			default:
+				break;
+		}
+	}
+	
+	[defaults setBool:YES forKey:@"hasStartedBefore"];
+	[defaults synchronize];
+}
+
+
+#pragma mark -
 #pragma mark General functions
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
 	/* Start network monitoring */
-	[[NetworkMonitor sharedNetworkMonitor] monitorNetwork]; 
+	[[NetworkMonitor sharedNetworkMonitor] monitorNetwork];
+	
+	if (![[NSUserDefaults standardUserDefaults] boolForKey:@"hasStartedBefore"]) {
+		[self firstLaunch];
+	}
 }
 
 - (void)applicationWillTerminate:(NSNotification *)notification {
@@ -28,6 +57,7 @@
 	NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
 	NSData *data = [NSKeyedArchiver archivedDataWithRootObject:self.serverListController.serverList];
 	[defaults setObject:data forKey:@"serverList"];
+	[defaults synchronize];
 }
 
 - (void)awakeFromNib {
@@ -37,8 +67,6 @@
 								 [NSNumber numberWithBool:YES], @"SUEnableAutomaticChecks",
 								 nil];
 	[defaults registerDefaults:appDefaults];
-	
-		//[self registerForLogin];
 }
 
 #pragma mark PreferenceWindowController
