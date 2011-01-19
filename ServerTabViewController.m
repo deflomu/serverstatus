b464205ed45229a4492b28eacb74a0c8bf534744
Florian Mutter <elm@skweez.net>
2011-01-19 17:20:24 +0100

    Set the status correct

--- ServerTabViewController.m
+++ ServerTabViewController.m
@@ -13,6 +13,29 @@
 @implementation ServerTabViewController
 @synthesize serverName, serverHost, lastKnownAddress, serverStatus, pingingIndicator, server;
 
+#pragma mark Private
+- (NSString *)getStatusString:(ServerStatus) aServerStatus {
+	NSString *status;
+	switch (aServerStatus) {
+		case SERVER_OK:
+			status = NSLocalizedString(@"Server is reachable",@"Server is reachable");
+			break;
+		case SERVER_FAIL:
+			status = NSLocalizedString(@"Server is unreachable",@"Server is unreachable");
+			break;
+		case SERVER_UNKNOWN:
+			status = NSLocalizedString(@"Server status is unknown",@"Server status is unknown");
+			break;
+		case SERVER_ERROR:
+			status = NSLocalizedString(@"An error occurred",@"An error occurred");
+			break;
+		default:
+			status = NSLocalizedString(@"No server status is set",@"No server status is set");
+			break;
+	}
+	return status;
+}
+
 - (void)setServer:(Server *)aServer {
 	[server removeObserver:self forKeyPath:@"lastKnownAddress"];
 	[server removeObserver:self forKeyPath:@"serverStatus"];
@@ -38,8 +61,8 @@
 	server = [aServer retain];
 }
 
-- (void)serverIsPinging:(Server *)aServer {
-	if ([aServer pinging]) {
+- (void)serverIsPinging:(BOOL)pinging {
+	if (pinging) {
 		[serverStatus setHidden:YES];
 		[pingingIndicator startAnimation:self];
 	} else {
@@ -68,15 +91,7 @@
 					  ofObject:(id)object
 						change:(NSDictionary *)change
 					   context:(void *)context {
-	if (keyPath == @"server") {
-		[self updateView];
-	}
-	if (keyPath == @"lastKnownAddress") {
-		[self updateView];
-	}
-	if (keyPath == @"pinging") {
-		[self serverIsPinging:object];
-	}
+	[self updateView];
 }
 
 - (void)updateView {
@@ -84,11 +99,15 @@
 		[self.serverName setStringValue:self.server.serverName];
 		[self.serverHost setStringValue:self.server.serverHost];
 		[self.serverHost setEnabled:YES];
+		[self serverIsPinging:[server pinging]];
+		[self.serverStatus setStringValue:[self getStatusString:[self.server serverStatus]]];
 		[self.lastKnownAddress setStringValue:[self.server lastKnownAddressAsString]];
 	} else {
 		[self.serverName setStringValue:@"Server"];
 		[self.serverHost setStringValue:@""];
 		[self.serverHost setEnabled:NO];
+		[self serverIsPinging:FALSE];
+		[self.serverStatus setStringValue:@""];
 		[self.lastKnownAddress setStringValue:@""];
 	}
 }
